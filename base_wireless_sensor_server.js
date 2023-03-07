3ca70859cc3b245d488423554be0bb8dcbc43038
Hüseyin GÜNEŞ <huseyingunes@gmail.com>
2023-03-07 07:30:36 +0300

    wireless_sensor_connection base codes

---
+++ base_wireless_sensor_server.js
@@ -0,0 +1,13 @@
+const { Server } = require("socket.io");
+
+const io = new Server({ /* options */ });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+  socket.on("example", (arg) => {
+	console.log(arg); // arg is a data from remote sensor
+  });
+});
+
+io.listen(3001);
