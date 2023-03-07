5aaf22e533b63595249828e600c2d91dd720649a
Hüseyin GÜNEŞ <huseyingunes@gmail.com>
2023-03-07 07:31:40 +0300

    bug fix

--- base_wireless_sensor_server.js
+++ base_wireless_sensor_server.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 const io = new Server({ /* options */ });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a remote sensor controller connected');
   
   socket.on("example", (arg) => {
 	console.log(arg); // arg is a data from remote sensor
